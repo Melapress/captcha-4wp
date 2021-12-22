389d2958ea80748dfac48edde8bfad147a91496d
DannyWPWS <DannyWPWS@users.noreply.github.com>
2021-12-22 11:34:00 +0000

    [create-pull-request] automated changes sync

---
+++ admin/templates/upgrade/index.php
@@ -0,0 +1,275 @@
+<div class="wrap features-wrap">
+	<div class="c4wp-upgrade-section">
+		<div class="title">
+			<h1><?php esc_html_e( 'Premium features', 'advanced-nocaptcha-recaptcha' ); ?></h1>
+		</div>
+
+		<div class="content-block">
+			<div class="logo-wrap">
+				<img src="<?php echo C4WP_PLUGIN_URL; ?>assets/img/features-image.png" alt="">
+			</div>
+			<p><?php esc_html_e( 'Improve the security of your WordPress logins and forms and block spambots from flooding your website. Use the flexible and customizable CAPTCHA 4WP to add CAPTCHA checks and protect your website from spam.', 'c4wp-wpp' ); ?></p>
+			<p><?php esc_html_e( 'Upgrade today for access to these plugin feature\'s and our first class premium support', 'c4wp-wpp' ); ?></p>
+			<div class="premium-cta">
+				<a href="https://www.wpwhitesecurity.com/wordpress-plugins/captcha-plugin-wordpress/pricing/" target="_blank" rel="noopener">UPGRADE NOW</a>
+			</div>
+		</div>
+
+		<div class="content-block">
+			<table class="c21 feature-table">
+				<tbody>
+					<tr class="c2">
+						<td class="c6" colspan="1" rowspan="1">
+							<p class="c10 c4"><span class="c5"></span></p>
+						</td>
+						<td class="c8 row-head" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5">Premium</span></p>
+						</td>
+						<td class="c12 row-head" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5">Free</span></p>
+						</td>
+					</tr>
+					<tr class="c2">
+						<td class="c6" colspan="1" rowspan="1">
+							<p class="c10"><span class="c5">Support</span></p>
+						</td>
+						<td class="c8" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5">Email &amp; forums</span></p>
+						</td>
+						<td class="c12" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5">Forums</span></p>
+						</td>
+					</tr>
+					<tr class="c2">
+						<td class="c6" colspan="1" rowspan="1">
+							<p class="c10"><span class="c5">WordPress login and registration protection</span></p>
+						</td>
+						<td class="c8" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+						<td class="c12" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+					</tr>
+					<tr class="c2">
+						<td class="c6" colspan="1" rowspan="1">
+							<p class="c10"><span class="c5">WordPress comments protection</span></p>
+						</td>
+						<td class="c8" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+						<td class="c12" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+					</tr>
+					<tr class="c2">
+						<td class="c6" colspan="1" rowspan="1">
+							<p class="c10"><span class="c5">Your choice of recaptcha v2, v2 checkbox or v3</span></p>
+						</td>
+						<td class="c8" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+						<td class="c12" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+					</tr>
+					<tr class="c2">
+						<td class="c6" colspan="1" rowspan="1">
+							<p class="c10"><span class="c5">WooCommerce login, registration and checkout protection</span></p>
+						</td>
+						<td class="c8" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+						<td class="c12" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-no"></span></span></p>
+						</td>
+					</tr>
+					<tr class="c2">
+						<td class="c6" colspan="1" rowspan="1">
+							<p class="c10"><span class="c5">Contact Form 7 form protection</span></p>
+						</td>
+						<td class="c8" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+						<td class="c12" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-no"></span></span></p>
+						</td>
+					</tr>
+					<tr class="c2">
+						<td class="c6" colspan="1" rowspan="1">
+							<p class="c10"><span class="c5">Hide CAPTCHA for logged in users</span></p>
+						</td>
+						<td class="c8" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+						<td class="c12" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-no"></span></span></p>
+						</td>
+					</tr>
+					<tr class="c2">
+						<td class="c6" colspan="1" rowspan="1">
+							<p class="c10"><span class="c5">Hide CAPTCHA based on role</span></p>
+						</td>
+						<td class="c8" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+						<td class="c12" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-no"></span></span></p>
+						</td>
+					</tr>
+					<tr class="c2">
+						<td class="c6" colspan="1" rowspan="1">
+							<p class="c10"><span class="c5">Hide CAPTCHA based on IP or URL</span></p>
+						</td>
+						<td class="c8" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+						<td class="c12" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-no"></span></span></p>
+						</td>
+					</tr>
+					<tr class="c2">
+						<td class="c6" colspan="1" rowspan="1">
+							<p class="c10"><span class="c5">Show CAPTCHA upon failed login</span></p>
+						</td>
+						<td class="c8" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+						<td class="c12" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-no"></span></span></p>
+						</td>
+					</tr>
+					<tr class="c2">
+						<td class="c6" colspan="1" rowspan="1">
+							<p class="c10"><span class="c5">BuddyPress comments, registration and group creation form protection</span></p>
+						</td>
+						<td class="c8" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+						<td class="c12" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-no"></span></span></p>
+						</td>
+					</tr>
+					<tr class="c2">
+						<td class="c6" colspan="1" rowspan="1">
+							<p class="c10"><span class="c5">bbPress new topic and reply to topicform protection</span></p>
+						</td>
+						<td class="c8" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-saved"></span></span></p>
+						</td>
+						<td class="c12" colspan="1" rowspan="1">
+							<p class="c7"><span class="c5"><span class="dashicons dashicons-no"></span></span></p>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+
+		<div class="premium-cta">
+			<a href="https://www.wpwhitesecurity.com/wordpress-plugins/captcha-plugin-wordpress/pricing/" target="_blank" rel="noopener">UPGRADE NOW</a>
+		</div>
+
+	</div>
+</div>
+
+<style>
+	.features-wrap {
+		background: #fff;
+		padding: 25px 30px;
+		max-width: 880px;
+	}
+
+	.features-wrap h2 {
+		font-size: 28px;
+			margin-bottom: 30px;
+	}
+
+	.features-wrap p {
+		font-size: 16px;
+		line-height: 28px;
+	}
+
+	.feature-list {
+		margin-bottom: 20px;
+	}
+
+	.feature-list li {
+		margin-bottom: 10px;
+		font-size: 15px;
+	}
+
+	.feature-list li .dashicons {
+		color: #3E6BFF;
+	}
+
+	.premium-cta {
+		margin: 25px 0 15px;
+	}
+
+	.premium-cta a, .table-link {
+		background-color: #3E6BFF;
+		color: #fff;
+		padding: 15px 26px;
+		border-radius: 30px;
+		font-size: 16px;
+		white-space: nowrap;
+		text-decoration: none;
+		font-weight: 700;
+		display: inline-block;
+		margin-right: 15px;
+		border: 2px solid #3E6BFF;
+	}
+
+	.premium-cta a:hover, .table-link:hover, .premium-cta a.inverse, .table-link.inverse {
+		color: #3E6BFF;
+		background-color: #fff;
+	}
+
+	.content-block {
+		margin-bottom: 26px;
+		border-bottom: 1px solid #eee;
+		padding-bottom: 15px;
+		overflow: hidden;
+	}
+	
+	.feature-table tr td {
+		text-align: center;
+		min-width: 200px
+	}
+	.feature-table tr td:first-of-type {
+		text-align: left;
+		font-weight: 500;
+	}
+	.feature-table td p {
+		margin-top: 0;
+	}
+	.row-head span {
+		font-size: 17px;
+		font-weight: 700;
+	}
+	.feature-table .dashicons {
+		color: #3E6BFF;
+	}
+	.feature-table .dashicons-no {
+		color: red;
+	}
+	.table-link {
+		font-size: 14px;
+		padding: 9px;
+		width: 193px;
+		margin-top: 10px;
+	}
+	.pull-up {
+		position: relative;
+		top: -23px;
+	}
+
+	.logo-wrap img {
+	    max-width: 230px;
+	}
+
+	.logo-wrap {
+		float: left;
+		margin-right: 30px;
+	}
+</style>
