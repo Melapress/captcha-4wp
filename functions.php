5d3295515761ea2f35152eab58269f4a905036c1
DannyWPWS <DannyWPWS@users.noreply.github.com>
2022-01-27 15:57:31 +0000

    [create-pull-request] automated changes sync

--- functions.php
+++ functions.php
@@ -3,15 +3,7 @@
 add_action( 'init', 'c4wp_plugin_update', -15 );
 
 function c4wp_plugin_update() {
-	$prev_version = c4wp_get_option( 'version' );
-	if ( ! $prev_version ) {
-		if ( is_multisite() ) {
-			update_site_option( 'c4wp_is_fresh_install', true );
-		} else {
-			update_option( 'c4wp_is_fresh_install', true );
-		}
-		$prev_version = '3.1';
-	}
+	$prev_version    = c4wp_get_option( 'version', '3.1' );
 	if ( version_compare( $prev_version, C4WP_PLUGIN_VERSION, '!=' ) ) {
 		do_action( 'c4wp_plugin_update', $prev_version );
 		c4wp_update_option( 'version', C4WP_PLUGIN_VERSION );
@@ -26,7 +18,9 @@ add_action( 'c4wp_plugin_update', 'c4wp_plugin_update_70', 30 );
 add_action( 'c4wp_plugin_update', 'c4wp_plugin_flag_upgrade', 10 );
 
 function c4wp_plugin_flag_upgrade( $prev_version ) {
-	if ( '3.1' != $prev_version && version_compare( $prev_version, '7.0.2', '<' ) ) {
+	$captcha_version = c4wp_get_option( 'captcha_version' );
+
+	if ( version_compare( $prev_version, '7.0.2', '<' ) ) {
 		if ( is_multisite() ) {
 			$upgrade_completed = get_site_option( 'c4wp_70_upgrade_notice_accepted' );
 			if ( ! $upgrade_completed ) {
